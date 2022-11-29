a833d87dec174f96f0e19690d3679b3dcc137631
Limon4egtop <91752425+Limon4egtop@users.noreply.github.com>
2022-11-29 21:30:00 +0300

    del global data

--- bot.py
+++ bot.py
@@ -3,10 +3,4 @@ from telebot import types
 from telebot.types import InputMediaPhoto
 
 
-global lang
-global CityID
-global PointID
-global ExcursionID
-global MassOfMemorables
-
 bot = telebot.TeleBot('5387821339:AAFALT-ceNAgR8PZ6IMUFr6FvtNim7zdZhM')
